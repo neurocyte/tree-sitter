6ba45950f62b5e66ab37d0616b7659c84340e5e4
CJ van den Berg <cj@vdbonline.com>
2024-02-21 13:36:19 +0100

    Add tree-sitter-xml

--- build.zig
+++ build.zig
@@ -61,6 +61,8 @@ pub fn build(b: *std.Build) void {
     addParser(b, lib, "typescript", "tsx");
     addParser(b, lib, "typescript", "typescript");
     addParser(b, lib, "verilog", null);
+    addParser(b, lib, "xml", "dtd");
+    addParser(b, lib, "xml", "xml");
     addParser(b, lib, "zig", null);
     b.installArtifact(lib);
     lib.installHeadersDirectory("tree-sitter/lib/include/tree_sitter", "tree_sitter");
