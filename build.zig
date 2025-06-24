1c3ad59bd98ee430b166054030dac4c46d641e39
CJ van den Berg <cj@vdbonline.com>
2025-06-24 12:19:21 +0200

    feat: add tree-sitter-powershell

--- build.zig
+++ build.zig
@@ -64,6 +64,7 @@ pub fn build(b: *std.Build) void {
         .{ "openscad", null },
         .{ "org", null },
         .{ "php", "php" },
+        .{ "powershell", null },
         .{ "proto", null },
         .{ "purescript", null },
         .{ "python", null },
